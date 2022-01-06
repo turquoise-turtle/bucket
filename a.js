6699b49c6fd67a214b1f979490ef0e2fe875baad
turquoise-turtle <16552560+turquoise-turtle@users.noreply.github.com>
2022-01-06 11:28:29 +1100

    serviceworker not service-worker

--- a.js
+++ a.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', function() {
-		navigator.serviceWorker.register('/bucket/service-worker.js')
+		navigator.serviceWorker.register('/bucket/serviceworker.js')
 		.then(function (reg){
 			console.log('sw registered:', reg);
 		}, /*catch*/ function(error) {
